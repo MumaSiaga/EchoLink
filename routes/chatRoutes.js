973d0a4d1203801b0e969e2042c71b356da51c09
MumaSiaga <mumandafhadzi@gmail.com>
2025-08-05 01:26:18 +0200

    updated profile page

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -27,18 +27,30 @@ router.get('/profile', ensureAuth, async (req, res) => {
 
   try {
     const fullUser = await User.findById(user._id).lean();
-      const chat = await Chat.findOne({ participants: user._id,isClosed: false});
+
+    
+    const now = Date.now();
+    const stories = (user.status || []).filter(s => (now - new Date(s.createdAt)) < 24 * 60 * 60 * 1000);
+
+    const chat = await Chat.findOne({ participants: user._id, isClosed: false });
     const userId = user._id;
     let partnerId = null;
     if (chat) {
       partnerId = chat.participants.find(p => !p.equals(userId));
     }
-  res.render('profile', { user: fullUser , partnerId: partnerId ? partnerId.toString() : null });
- 
+    const latestStory = stories.length > 0 ? stories[stories.length - 1] : null;
+    res.render('profile', { 
+      user: fullUser, 
+      partnerId: partnerId ? partnerId.toString() : null,
+      stories: stories || [] ,
+      latestStory: latestStory || null
+    });
+
   } catch (err) {
     console.error('Error fetching profile:', err);
     res.status(500).send('Error loading profile');
   }
 });
+
 
 module.exports = router;
